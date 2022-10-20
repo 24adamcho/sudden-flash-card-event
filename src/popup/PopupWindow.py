bd7de33de26921201586e67d58ed30b3113f58eb
Adam Cho <not@an.email>
2022-10-20 16:57:13 -0500

    colors!

--- src/popup/PopupWindow.py
+++ src/popup/PopupWindow.py
@@ -3,40 +3,85 @@ from tkinter import ttk
 
 import quizlogic
 
+class PopupWindowFrame(tk.Frame):
+    def __init__(self, parent, card, answer):
+        print("Frame")
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.__card__ = card
+        self.__answer__ = answer
+        self.widgets()
+        
+    def widgets(self):
+        print("frame widgets")
+        self.lbl_description = ttk.Label(self, text="You must answer.")
+        self.lbl_card = ttk.Label(self, text = self.__card__)
+        self.ent_guess = ttk.Entry(self)
+        self.lbl_answer = tk.Label(self) #TK ALLOWS FG/BG CONTROL, NOT TTK
+
+        self.lbl_description.pack()
+        self.lbl_card.pack()
+        self.ent_guess.pack()
+        self.lbl_answer.pack()
+        print("frame widgets packed")
+
+    def getEntryText(self):
+        return self.ent_guess.get()
+
+    def correct(self):
+        self.lbl_answer.config(text="Correct!")
+        self.lbl_answer.config(fg="green")
+    def incorrect(self):
+        self.lbl_answer.config(text=f"Wrong! The answer was [ {self.__answer__} ].")
+        self.lbl_answer.config(fg="red")
+
 class PopupWindow(tk.Tk):
     __quizCompleted__ = False
 
-    def __init__(self, cards, quizlogic, options = {"windowSize":"300x300"}):
+    def __init__(self, cards, ql, options = {"windowSize":"350x100"}):
         print("Initializing window...")
         super().__init__()
 
         self.title("SUDDEN FLASH CARD EVENT")
         self.geometry(options["windowSize"]) #TODO add windowSize to cfg.json
 
-        self.__logic__ = quizlogic.QuizLogic()
-
-        self.lbl_description = ttk.Label(self, text="You must answer.")
-        self.lbl_card = ttk.Label()
-        self.ent_guess = ttk.Entry()
-        self.lbl_answer = ttk.Label()
+        self.__logic__ = quizlogic.QuizLogic(cards)
         
         self.bind('<Return>', self.onNewlineEvent)
         self.protocol("WM_DELETE_WINDOW", self.onClose)
 
-        self.lbl_description.pack()
-        self.lbl_card.pack()
-        self.ent_guess.pack()
-        self.lbl_answer.pack()
+        self.frames = list()
+        for card, answer in cards.items():
+            self.frames.append(PopupWindowFrame(self, card, answer))
+
+        self.frame = PopupWindowFrame(self, "test", "water")
+
+        self.frame.pack()
         
         print("Window initialized")
     
     def onNewlineEvent(self, event):
         print("Entry submitted")
+        if self.__logic__.guess(self.frame.getEntryText()):
+            self.frame.correct()
+        else:
+            self.frame.incorrect()
+        
+        if self.__logic__.nextCard():
+            pass #change to next frame
+        else:
+            self.onClose() #close window and return score
 
     def onClose(self):
         print("Window closed!")
         self.destroy()
 
 if __name__ == "__main__":
-    app = PopupWindow("", "")
+    cards = {
+        "test": "answer",
+        "mock": "dancer"
+    }
+    logic = quizlogic.QuizLogic(cards)
+    app = PopupWindow(cards, logic)
     app.mainloop()
+    print("loop exited")
--- src/popup/QuizLogic.py
+++ src/popup/QuizLogic.py
@@ -8,18 +8,33 @@ class QuizLogic(object):
         self.__guessIndex__ = 0
         self.__score__ = 0
 
+    # guess the card's answer
+    #   returns:
+    #       True - answer is correct
+    #              increments score
+    #       False - answer was incorrect
     def guess(self, card):
         if card == self.peekAnswer():
+            self.__score__ += 1
             output = True
         else:
             output = False
+        return output    
+
+    # next Card increments the card counter
+    #   returns:
+    #       True - if the current index is valid
+    #       False - if the current index is out of bounds
+    def nextCard(self):
         self.__guessIndex__ += 1
-        return output
+        if self.__guessIndex__ >= len(self.__cards__):
+            return False
+        return True
 
     def results(self):
         return self.__score__
 
     def peekAnswer(self):
-        return self.__cards__.values()[self.__guessIndex__]
+        return list(self.__cards__.values())[self.__guessIndex__]
     def peekCard(self):
-        return self.__cards__.keys()[self.__guessIndex__]
+        return list(self.__cards__.keys())[self.__guessIndex__]
