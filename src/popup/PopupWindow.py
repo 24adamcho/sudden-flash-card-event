216b8d1221e4164466f1883dac50d2f30e8cc196
Adam Cho <not@an.email>
2022-10-21 15:01:57 -0500

    removed test cases

--- src/popup/PopupWindow.py
+++ src/popup/PopupWindow.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
         "hell": "water",
         "high": "water"
     }
-    logic = quizlogic.QuizLogic(cards)
-    app = PopupWindow(logic, "You must answer.")
-    app.mainloop()
+    #logic = quizlogic.QuizLogic(cards)
+    #app = PopupWindow(logic, "You must answer.")
+    #app.mainloop()
     print("loop exited")
